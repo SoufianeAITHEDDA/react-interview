5120555c38705a8cccc880b92e4b4a096070107f
Soufiane AIT HEDDA <387-as193914@users.noreply.www-apps.univ-lehavre.fr>
2021-10-20 19:05:54 +0200

    correction import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './component/App';
+import App from './Component/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
